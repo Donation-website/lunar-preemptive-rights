18d53a1660bd054835471ccb9526263344f997fc
Donation-website <lipcseitundetimea@gmail.com>
2026-01-17 20:19:06 +0100

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,13 +1,18 @@
 export default function Footer() {
   return (
-    <footer>
-      <p>No ownership is granted under current law (Outer Space Treaty, 1967)</p>
-      <div className="socials">
-        <img src="/fb.png" alt="Facebook" onClick={() => window.open('https://facebook.com')} />
-        <img src="/insta.png" alt="Instagram" onClick={() => window.open('https://instagram.com')} />
-        <img src="/x.png" alt="X" onClick={() => window.open('https://x.com')} />
+    <footer style={{ textAlign: "center", padding: "20px", background: "#111", color: "#fff" }}>
+      <div style={{ marginBottom: "10px" }}>
+        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" style={{ margin: "0 10px" }}>
+          <img src="/icons/facebook.svg" alt="FB" width="24"/>
+        </a>
+        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" style={{ margin: "0 10px" }}>
+          <img src="/icons/instagram.svg" alt="IG" width="24"/>
+        </a>
+        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" style={{ margin: "0 10px" }}>
+          <img src="/icons/twitter.svg" alt="X" width="24"/>
+        </a>
       </div>
-      <p>© 2026 Lunar Pre-Emptive Rights – Conceptual Initiative</p>
+      <div>© 2026 Lunar Pre-Emptive Rights. Conceptual initiative.</div>
     </footer>
-  )
+  );
 }
