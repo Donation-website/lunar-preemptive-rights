74e99b232fcb9883821f5027c28b2c80ee6ce4d8
Donation-website <lipcseitundetimea@gmail.com>
2026-01-18 13:07:29 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main style={{
+      minHeight: '100vh',
+      background: '#000',
+      color: '#fff',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontFamily: 'Arial'
+    }}>
+      <h1>🌕 Lunar Pre-Emptive Rights – LIVE</h1>
+    </main>
+  );
+}
