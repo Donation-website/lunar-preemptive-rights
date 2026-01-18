e7331d13dc245c955af6a182b9a0f7a717a4957c
Donation-website <lipcseitundetimea@gmail.com>
2026-01-18 14:06:00 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,11 @@
+// pages/index.js
 import dynamic from 'next/dynamic'
 
-const MoonScene = dynamic(
-  () => import('../components/MoonScene'),
-  { ssr: false }
-)
+const MoonScene = dynamic(() => import('../components/MoonScene'), { ssr: false })
 
 export default function Home() {
   return (
-    <div style={{ margin: 0, padding: 0 }}>
+    <div style={{ margin: 0, padding: 0, overflow: 'hidden' }}>
       <h1 style={{
         position: 'absolute',
         zIndex: 10,
@@ -16,7 +14,6 @@ export default function Home() {
       }}>
         🌕 Lunar Pre-Emptive Rights – LIVE
       </h1>
-
       <MoonScene />
     </div>
   )
