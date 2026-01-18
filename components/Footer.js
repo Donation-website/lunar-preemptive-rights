bc4f20ed6ebf925d34e85cb4839bf37b9c58900e
Donation-website <lipcseitundetimea@gmail.com>
2026-01-18 05:09:03 +0100

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div>
+        <a href="https://facebook.com" target="_blank">FB</a>
+        <a href="https://instagram.com" target="_blank">IG</a>
+        <a href="https://twitter.com" target="_blank">X</a>
+      </div>
+      <p>© 2026 Lunar Pre-Emptive Rights. Conceptual initiative.</p>
+    </footer>
+  );
+}
