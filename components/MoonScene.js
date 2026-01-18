4056068db47ae96473ee758a1b9df3c63fb2b5f6
Donation-website <lipcseitundetimea@gmail.com>
2026-01-18 14:05:14 +0100

    Update MoonScene.js

--- components/MoonScene.js
+++ components/MoonScene.js
@@ -1,20 +1,25 @@
-import React, { Suspense } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Sphere, MeshStandardMaterial } from '@react-three/drei';
+// components/MoonScene.js
+import React, { Suspense } from 'react'
+import * as THREE from 'three'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
 
 export default function MoonScene() {
+  const texture = new THREE.TextureLoader().load('/moon/moon-map.jpg')
+
   return (
     <Canvas style={{ height: '100vh', width: '100vw' }} camera={{ position: [0, 0, 3] }}>
       <ambientLight intensity={0.5} />
       <pointLight position={[10, 10, 10]} />
       
       <Suspense fallback={null}>
-        <Sphere args={[1, 64, 64]}>
-          <MeshStandardMaterial map={new THREE.TextureLoader().load('/moon/moon-map.jpg')} />
-        </Sphere>
+        <mesh>
+          <sphereGeometry args={[1, 64, 64]} />
+          <meshStandardMaterial map={texture} />
+        </mesh>
       </Suspense>
 
-      <OrbitControls />
+      <OrbitControls enablePan={false} />
     </Canvas>
   )
 }
